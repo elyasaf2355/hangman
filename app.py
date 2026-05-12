bcbff720c3d83b167058e14b2919978298a7b5d3
ElyasafM <elyasaf2355@gmail.com>
2026-05-12 17:33:51 +0300

    inital setup

---
+++ app.py
@@ -0,0 +1,27 @@
+import random
+#variables
+words_list = ["apple", "banana", "cherry", "mango", "orange"]
+tries_number = 10
+word = ""
+x_word = ""
+#cli methods
+def show_word():
+    pass
+def get_char():
+    pass
+def success_message():
+    pass
+def fail_message():
+    pass
+#flow methods
+def choose_word():
+    random.choice(words_list)
+
+
+#logic methods
+def in_word():
+    pass
+def x_word_update():
+    pass
+def word_complete():
+    pass
