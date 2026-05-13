d269bf59ab5e820fd82cb0be1bed3ae885a8a0d7
ElyasafM <elyasaf2355@gmail.com>
2026-05-13 15:46:44 +0300

    first working app version

--- app.py
+++ app.py
@@ -1,27 +1,59 @@
 import random
 #variables
-words_list = ["apple", "banana", "cherry", "mango", "orange"]
-tries_number = 10
-word = ""
-x_word = ""
-#cli methods
-def show_word():
-    pass
-def get_char():
-    pass
-def success_message():
-    pass
-def fail_message():
-    pass
-#flow methods
-def choose_word():
-    random.choice(words_list)
+WORDS_LIST = ["apple", "banana", "cherry", "mango", "orange"]
+MAX_TRIES = 5
+#init
+def word_init(word_list):
+    word = random.choice(word_list)
+    word_x = ["_"] * len(word)
+    return word, word_x
 
+#cli methods
+def show_word(word_x):
+    print(f"Word to guess: {"".join(word_x)}")
+def show_tries(left_tries):
+    print(f"Tries left: {left_tries}")
+def get_char():
+    return input("Enter char: ")
+def success_message():
+    print("\nG-R-A-T-U-L-A-T-I-O-N-S-!\n You win the game")
+def fail_message():
+    print("\nG-O-O-D-B-Y-E   L-O-S-E-R..!\nMaybe next time")
 
 #logic methods
-def in_word():
-    pass
-def x_word_update():
-    pass
-def word_complete():
-    pass
+def in_word(char, word):
+    return char.lower() in word
+def x_word_update(word_x, word, char):
+    for i in range(len(word)):
+        if word[i] == char:
+            word_x[i] = char
+    return word_x
+def word_complete(word_x):
+    return not '_' in word_x
+
+#app flow
+def app(word_list, max_tries):
+#init
+    #guessed_chars to prevent duplicate chars tries
+    guessed_chars = []
+    word, word_x = word_init(word_list)
+    left_tries = max_tries
+    while left_tries > 0 and not word_complete(word_x):
+        show_tries(left_tries)
+        show_word(word_x)
+        user_char = get_char()
+        guessed_chars.append(user_char)
+        if in_word(user_char, word):
+            word_x = x_word_update(word_x, word, user_char)
+        else:
+            left_tries -= 1
+    #end game
+    if word_complete(word_x):
+        success_message()
+    else:
+        fail_message()
+
+#app
+if __name__ == '__main__':
+    app(WORDS_LIST, MAX_TRIES)
+
